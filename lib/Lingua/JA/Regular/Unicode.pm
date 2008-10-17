cf14974021d326972f29eacf5649d7980c6c4873
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-10-17 11:41:05 +0000

    Checking in changes prior to tagging of version 0.01_01.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 29695)
    +++ Changes	(local)
    @@ -1,4 +1,5 @@
     Revision history for Perl extension Lingua::JA::Regular::Unicode
     
    -0.01    Fri Oct 17 16:35:25 2008
    -        - original version
    +0.01_01  Fri Oct 17 16:35:25 2008
    +        - development release
    +
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Lingua-JA-Regular-Unicode/trunk@21529 d0d07461-0603-4401-acd4-de1884942a52

--- lib/Lingua/JA/Regular/Unicode.pm
+++ lib/Lingua/JA/Regular/Unicode.pm
@@ -2,7 +2,7 @@ package Lingua::JA::Regular::Unicode;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '0.01';
+our $VERSION = '0.01_01';
 use Exporter 'import';
 
 our @EXPORT = qw/ hiragana2katakana alnum_z2h space_z2h /;
